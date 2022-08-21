6b52d21e2b54c358fc51027717b49aaa4ab86ddf
andrewluooo <protonluo@protonmail.com>
2022-08-21 17:34:15 +0800

    first easy one

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
+pub mod easy_one;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use crate::easy_one::List;
+    use super::easy_one::List;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
     }
 }
